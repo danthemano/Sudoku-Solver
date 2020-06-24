2848ef3599b9bad3e6180b655165a14f217e3eca
Dan <danm.benett@gmail.com>
2020-06-24 14:19:39 -0500

    added program files

---
+++ SudokuSolver.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<String>();
+
+        //checks each row for a copy of an element
+        for(int i = 0; i < 9; i++){
+
+            //checks each column for a copy of an element
+            for(int j = 0; j < 9; j++){
+
+                //adds the element in each row, col, and box with a specific string
+                //to indetify if the element is reapeated in that specific row, col, or box
+                if( board[i][j] != '.'){
+                    if(!seen.add(board[i][j] + "row" + i) ||
+                            !seen.add(board[i][j] + "col" + j) ||
+                            !seen.add(board[i][j] + "box" + i/3 + "-" + j/3))
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
